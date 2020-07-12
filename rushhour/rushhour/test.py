f5e405f080abc5d991d9bb7ea6e6b448a9114d14
Finn Rub <r.finn40@yahoo.com>
2020-07-12 16:31:50 +0200

    pip install psycopg2

--- rushhour/rushhour/test.py
+++ rushhour/rushhour/test.py
@@ -1,5 +1,12 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import requests
+
 token = os.environ.get("TOKEN")
 print(token)
+
+url = 'http://127.0.0.1:3000/api/v1/db_url'
+headers = {'token': token}
+r = requests.get(url, headers=headers)
+print(r.text)
